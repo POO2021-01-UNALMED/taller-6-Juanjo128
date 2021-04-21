9cd7dc4d74894204373b3f30e4ab4158603e6b5c
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-04-20 21:21:16 -0500

    Create Automovil.java

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,27 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo{
+	public int puestos;
+	
+	
+	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos = puestos;
+		numAutomovil++;
+	}
+	
+	public void setPuestos(int x) {
+		this.puestos = x;
+	}
+	
+	public int getPuestos() {
+		return this.puestos;
+	}
+	
+
+	public  int getCantidadAutomoviles() {
+		return numAutomovil;
+	}
+	
+
+}
