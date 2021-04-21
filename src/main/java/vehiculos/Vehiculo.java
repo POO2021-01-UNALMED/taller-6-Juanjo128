3a9e111d59555f53387f6d7955d2308af20aef65
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-04-20 21:21:57 -0500

    Create Vehiculo.java

---
+++ src/main/java/vehiculos/Vehiculo.java
@@ -0,0 +1,99 @@
+package vehiculos;
+
+public class Vehiculo {
+	public String placa;
+	public int puertas;
+	public int velocidadMaxima;
+	public String nombre;
+	public int precio;
+	public int peso;
+	public String traccion;
+	public Fabricante fabricante;
+	public static int numVehiculos;
+	public int numAutomovil;
+	public int numCamion;
+	public int numCamioneta;
+	
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante) {
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		numVehiculos++;
+		fabricante.nvehiculos++;
+	}
+	
+	public String getPlaca() {
+		return this.placa;
+	}
+	
+	public void setPuertas(int x) {
+		this.puertas = x;
+	}
+	
+	public int getPuertas() {
+		return this.puertas;
+	}
+	
+	public void setVelocidadMaxima(int x) {
+		this.velocidadMaxima = x;
+	}
+	
+	public int getVelocidadMaxima() {
+		return this.velocidadMaxima;
+	}
+	
+	public void setPrecio(int x) {
+		this.precio = x;
+	}
+	
+	public int getPrecio(int x) {
+		return this.precio;
+	}
+	
+	public void setPeso(int x) {
+		this.peso = x;
+	}
+	
+	public int getPeso() {
+		return this.peso;
+	}
+	
+	public void setNombre(String y) {
+		this.nombre = y;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setTraccion(String y) {
+		this.traccion = y;
+	}
+	
+	public String getTraccion() {
+		return this.traccion;
+	}
+	
+	public void setFabricante(Fabricante y) {
+		this.fabricante = y;
+	}
+	
+	public Fabricante getFabricante() {
+		return this.fabricante;
+	}
+	
+	public static int getCantidadVehiculos() {
+		return numVehiculos;
+	}
+	
+	public String vehiculosPorTipo() {
+		return("Automoviles:" + numAutomovil + "\n" + "Camionetas:" + numCamioneta + "\n" + "Camionetas:" + numCamion );
+	}
+	
+	
+}
